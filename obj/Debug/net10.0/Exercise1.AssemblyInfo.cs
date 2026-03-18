bd6edf0952ed61c26545940a852f73187ef33f32
Pieter Van Der Berg <pietervanderberg@gmail.com>
2026-03-18 10:47:58 +0100

    Exercise 1 - Basics and Development Environment

--- obj/Debug/net10.0/Exercise1.AssemblyInfo.cs
+++ obj/Debug/net10.0/Exercise1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baff197d025b6d75eab9ad7cf217f9cc32831fb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
